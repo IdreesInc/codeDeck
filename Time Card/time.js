67e0c38edf312c736d351a504edb42cfe6f5603f
Idrees Hassan <idrees@idreesinc.com>
2016-09-02 00:08:20 -0400

    Added dynamic time to time card.

--- Time Card/time.js
+++ Time Card/time.js
@@ -1,25 +1,65 @@
 var canvas;
 var ctx;
-var time = 0;
+var seconds = 0;
 
 $(document).ready(function() {
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 	var loop = function(){
-		time += 1/600;
+		update();
 		draw();
 	};
-	var interval = setInterval(loop, 100);
+	var interval = setInterval(loop, 10);
 	draw();
 });
 
+function update() {
+	var d = new Date();
+	seconds = d.getSeconds() / 59 + d.getMilliseconds() / 999 * (1 / 59);
+}
+
 function draw() {
+	var circleLoc = {x: canvas.width / 1.6, y: canvas.height / 2};
+	var textY = circleLoc.y + 30;
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	var d = new Date();
+	ctx.font = "100px calibri";
+	ctx.textAlign = "center";
+	for (var i = -3; i <= 3; i++) {
+		if (d.getMinutes() - i > -1) {
+			var trans = 100 * i + 100 * d.getSeconds() / 59 - 50;
+			//RGBA(255, 181, 107, 1.00)
+			ctx.fillStyle = 'RGBA(255, 181, 107, ' + (1 - Math.abs(trans) / 230) + ')';
+			var minutes = "" + d.getMinutes() - i;
+			if (d.getMinutes() - i < 10) {
+				minutes = "0" + minutes;
+			}
+			ctx.fillText(minutes, circleLoc.x, textY + trans);
+		}
+	}
+	ctx.font = "75px calibri";
+	ctx.fillStyle = 'RGBA(255, 181, 107, 1.00)';
+	ctx.textAlign = "left";
+	var hours = d.getHours();
+	if (hours > 12) {
+		hours -= 12;
+	} else if (hours === 0) {
+		hours = 12;
+	}
+	ctx.fillText(hours, 20, textY - 5);
+
 	ctx.beginPath();
-	ctx.arc(canvas.width / 2, canvas.height / 3, 100, Math.PI * 1.5, Math.PI * ((time * 2) + 1.5));
-	var gradient = ctx.createLinearGradient(0, 0, 0, 200);
-	gradient.addColorStop("0", "#FF5F6D");
-	gradient.addColorStop("1", "#FFC371");
+	ctx.arc(circleLoc.x, circleLoc.y, 90, Math.PI * 1.5, Math.PI * (2 + 1.5));
+	ctx.strokeStyle = "rgba(255,180,122, 0.2)";
+	ctx.lineWidth = 15;
+	ctx.lineCap = "round";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.arc(circleLoc.x, circleLoc.y, 90, Math.PI * 1.5, Math.PI * ((seconds * 2) + 1.5));
+	var gradient = ctx.createLinearGradient(0, canvas.height / 5, 0, canvas.height / 4 * 3);
+	gradient.addColorStop("0", "#FF976D");
+	gradient.addColorStop("1", "#FFCE52");
 	ctx.strokeStyle = gradient;
 	ctx.lineWidth = 15;
 	ctx.lineCap = "round";
